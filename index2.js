8d544bec8a6c22ce0454d511f20c2586ea6d7dfd
Finley <Finley.mwachia12@gmail.com>
2025-01-31 10:11:25 +0300

    add a new file and code for bubble sort

---
+++ index2.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr){
+    let swapped;
+    do{
+        swapped = false;
+        for(let i =0;i<arr.length-1;i++){
+            if(arr[i]> arr[i+1]){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+        }
+    }while(swapped)
+}
+let arr = [64, 34, 25, 12, 22, 11, 90];
+bubbleSort(arr);
+console.log(arr)
